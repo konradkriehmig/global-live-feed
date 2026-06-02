60f0527c9c6c062b0ca1b48575798c9a3c432423
konradkriehmig <konrad.kriehmig@insead.edu>
2026-06-02 14:36:52 +0100

    .

---
+++ infra/kafka/deploy-kafka.ps1
@@ -0,0 +1,3 @@
+kubectl get crds | Select-String "strimzi"
+
+kubectl apply -f infra\kafka\kafka.yaml
--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,15 @@
 python -m venv venv
 venv\Scripts\activate
 pip install -r requirements.txt
+
+winget install Helm.Helm
+az aks get-credentials --resource-group rg-globallivefeed-prod-westeu --name aks-globallivefeed-prod-westeu
+
+helm uninstall kafka --namespace kafka
+
+helm repo add strimzi https://strimzi.io/charts/
+helm repo update
+helm install strimzi strimzi/strimzi-kafka-operator --namespace kafka --create-namespace
+
+kubectl get pods --all-namespaces
+kubectl get pods --namespace kafka
