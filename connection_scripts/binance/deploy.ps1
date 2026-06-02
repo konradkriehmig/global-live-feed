abca45495e3f312e08fe595e9cbfa82c3ac37ee9
konradkriehmig <konrad.kriehmig@insead.edu>
2026-06-02 15:53:57 +0100

    .

--- connection_scripts/binance/deploy.ps1
+++ connection_scripts/binance/deploy.ps1
@@ -2,3 +2,7 @@ az acr login --name acrgloballivefeed
 
 docker build -t acrgloballivefeed.azurecr.io/binance-connector:latest connection_scripts/binance
 docker push acrgloballivefeed.azurecr.io/binance-connector:latest
+
+kubectl apply -f connection_scripts/binance/binance_connector.yaml
+
+kubectl get pods
